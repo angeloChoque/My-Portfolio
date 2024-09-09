f7757bd5c6826cdc831a9661b8e6cb20d25f9293
angelo <ancricoma_angelo@hotmail.com>
2024-09-08 21:06:10 -0500

    added icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
-import { Container, Grid } from "@mui/material";
+import { Container, Grid, Link, Stack } from "@mui/material";
 import { motion } from "framer-motion";
 import { COLOR_2 } from "../assets/color";
 import { useTranslation } from "react-i18next";
+import GitHub from "../assets/icons/GitHub";
+import Linkedin from "../assets/icons/Linkedin";
+import { Email } from "@mui/icons-material";
 
 const Home = () => {
   const { t } = useTranslation();
@@ -23,14 +26,14 @@ const Home = () => {
           </motion.h1>
           <motion.h2
             transition={{ duration: 0.6 }}
-            animate={{ y: 120 }}
+            animate={{ y: 90 }}
             style={{ fontSize: 38, fontWeight: "normal" }}
           >
             {t("home.hello_world")}
           </motion.h2>
           <motion.h2
             transition={{ duration: 0.5 }}
-            animate={{ y: 90 }}
+            animate={{ y: 60 }}
             style={{ fontSize: 38, fontWeight: "normal", alignItems: "center" }}
           >
             {t("home.introduction")}
@@ -38,11 +41,24 @@ const Home = () => {
           </motion.h2>
           <motion.h2
             transition={{ duration: 0.4 }}
-            animate={{ y: 60 }}
+            animate={{ y: 30 }}
             style={{ fontSize: 38, fontWeight: "normal" }}
           >
             {t("home.job_title")}
           </motion.h2>
+          <Stack
+            direction={"row"}
+            my={10}
+            spacing={1}
+            alignItems={"center"}
+            ml={15}
+          >
+            <GitHub />
+            <Linkedin />
+            <Link href="mailto:ancricoma_angelo@hotmail.com">
+              <Email sx={{ fontSize: 40, color: "#a19e9a" }} />
+            </Link>
+          </Stack>
         </Grid>
         <Grid component={"figure"} item xs={12} md={6} mt={10}>
           <img src="/My-Portfolio/imagen.avif" style={{ maxWidth: "100%" }} />
